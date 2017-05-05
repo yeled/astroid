ca7e2c34e23250008cacb3e37ee0751ef2e5f4db
Gaute Hope <eg@gaute.vetsj.com>
2017-05-05 10:01:15 +0200

    ti: use background color to indicate marked messages

--- src/modes/thread_index/thread_index_list_cell_renderer.hh
+++ src/modes/thread_index/thread_index_list_cell_renderer.hh
@@ -103,6 +103,8 @@ namespace Astroid {
       ustring subject_color; // configurable
       ustring subject_color_selected; // configurable
       ustring background_color_selected; // configurable
+      ustring background_color_marked          = "#cc3366";
+      ustring background_color_marked_selected = "#751d3b";
 
       void render_background (
           const ::Cairo::RefPtr< ::Cairo::Context>&cr,
